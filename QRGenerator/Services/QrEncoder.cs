be904dc4896b1bdc925dd484b80abd5317b539f6
Daniel Tierney <daniel.tierney1@icloud.com>
2024-12-12 10:38:33 +0000

    Updated QrEncoder.cs
    Added matrix information and finder patters

--- QRGenerator/Services/QrEncoder.cs
+++ QRGenerator/Services/QrEncoder.cs
@@ -9,6 +9,30 @@ public class QrEncoder
 
     public Bitmap GenerateQRCode(string content)
     {
+        if (string.IsNullOrWhiteSpace(content))
+        {
+            throw new ArgumentException("Input cannot be null or whitespace", nameof(content));
+        }
+
+        int matrixSize = 177; // Sets the size of the QR Code. This uses Ver.44 of the Qr Standard
         
+        bool[,] matrix = new bool[matrixSize, matrixSize]; // Initialise the matrix
+        
+        // Adding Finder Patterns
+        AddFinderPattern(matrix, 0, 0);
+        AddFinderPattern(matrix, matrixSize - 7, 0);
+        AddFinderPattern(matrix, 0, matrixSize - 7);
+    }
+    
+    // Method for adding finder patters to the matrix
+    private void AddFinderPattern(bool[,] matrix, int startX, int startY)
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                matrix[startX + i, startY + j] = (i == 0 || i == 6 || j == 0 || j == 6 || (i >= 2 && i <= 4 && j >= 2 && j <= 4));
+            }
+        }
     }
 }
